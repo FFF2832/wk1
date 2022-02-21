9c661ed93748ea82458e2d74b0a761dbb1282012
fiona <fiona.w32@gmail.com>
2022-02-21 17:05:37 +0800

    version2

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.a}>
+      <Text  style={styles. contentB}>110919030汪怡廷</Text>
+      <Text  style={styles. contentB}>終於做完了好想哭</Text>
+      <Image source = {{uri:'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg'}}
+   style = {{ width: 250, height: 250}}
+   />
+      </View>
+      
+    
     </View>
   );
 }
@@ -13,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C3AC9D',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  a:{
+    width: '100%',
+    paddingBottom:50,
+  },
+  
+  contentB: {
+    width: '100%',
+    fontSize: 30,
+    textAlign: 'left',
+    color: "#865754"
+  },
+  
 });
